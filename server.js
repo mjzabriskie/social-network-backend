49588e350a0078322fff00e7b62497590c7345a6
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-07-17 16:41:34 -0600

    initial commit, added basic file structure, created server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+mongoose.connect('mongodb://localhost:27017/social-network-backend', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Now connected on localhost:${PORT}`));
